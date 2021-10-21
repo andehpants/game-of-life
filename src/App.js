c9a36a324e9be695e5fa224443def1e0aaa50f83
Andy Zheng <andy.zheng@team.telstra.com>
2021-10-22 09:34:37 +1100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback, useRef } from "react";
+import produce from "immer";
+
+const numRows = 50;
+const numCols = 50;
 
 function App() {
+  const [running, setRunning] = useState(false);
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+    return rows;
+  });
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+  const gridClick = (i, k) => {
+    const newGrid = produce(grid, (gridCopy) => {
+      gridCopy[i][k] = !gridCopy[i][k];
+    });
+    setGrid(newGrid);
+  };
+
+  const simulate = useCallback(() => {
+    // Using a ref ensures the value is up to date in the callback
+    if (!runningRef.current) {
+      return;
+    }
+
+    setTimeout(simulate, 1000);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button
+        onClick={() => {
+          setRunning(!running);
+          simulate();
+        }}
+      >
+        {running ? "stop" : "start"}
+      </button>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: `repeat(${numCols},20px)`,
+        }}
+      >
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              key={`${i}-${k}`}
+              onClick={() => {
+                gridClick(i, k);
+              }}
+              style={{
+                width: 18,
+                height: 20,
+                backgroundColor: grid[i][k] ? "pink" : undefined,
+                border: "solid 1px black",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 }
 
